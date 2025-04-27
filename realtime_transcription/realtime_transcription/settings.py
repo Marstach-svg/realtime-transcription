354196d2e6e32c69649c7f02910482baea3e2ada
root <root@JARVIS.>
2025-04-27 18:10:49 +0900

    tailwindcssの初期設定とuserアプリの追加、urls.pyの編集

--- realtime_transcription/realtime_transcription/settings.py
+++ realtime_transcription/realtime_transcription/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'transcript.apps.TranscriptConfig',
+    'transcript',
+    'user',
 ]
 
 MIDDLEWARE = [
--- realtime_transcription/realtime_transcription/urls.py
+++ realtime_transcription/realtime_transcription/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('realtime/', )
+    path('realtime/', include('transcript.urls')),
+    path('user/', include('user.urls')),
 ]
